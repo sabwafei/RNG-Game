6682e79377d45798bb6ccd9b23143f17ed3815b2
Christopher Smith <57920513+sabwafei@users.noreply.github.com>
2021-01-07 11:40:34 -0500

    Initial project setup

---
+++ RNG game/src/main/Player.java
@@ -0,0 +1,11 @@
+//Created by: Christopher Smith
+//Player class defines the user playing the RNG game. Class contains the attributes
+//		Points	 
+//		Ticket Invetory
+//		Item Inventory
+
+package main;
+
+public class Player {
+
+}
